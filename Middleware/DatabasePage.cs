bf6ecd42963eae1775209f695dbb77adc74b24c4
anloffredo <loffredo.ap@gmail.com>
2019-10-23 00:56:56 -0700

    Framework extraction update
    Framework extraction update

---
+++ Middleware/DatabasePage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Penguin.Cms.Pages.Repositories;
+using Penguin.Web.Abstractions.Interfaces;
+using System.Threading.Tasks;
+
+namespace Penguin.Cms.Modules.Pages.Middleware
+{
+    //http://azurecoder.net/2017/07/09/routing-middleware-custom-irouter/
+    public class DatabasePage : IPenguinMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        //TODO: Learn what this is
+        public DatabasePage(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context is null)
+            {
+                throw new System.ArgumentNullException(nameof(context));
+            }
+
+            string RequestUrl = context.Request.Path.Value.Split('?')[0];
+
+            if (context.RequestServices.GetService<PageRepository>().TryGetPageFromCache(RequestUrl, out _))
+            {
+                context.Request.Path = "/Page/RenderPage";
+
+                context.Request.QueryString = context.Request.QueryString.Add("Url", RequestUrl);
+            }
+
+            await this._next(context).ConfigureAwait(true);
+        }
+    }
+}
---
+++ Rendering/PageRenderer.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Penguin.Cms.Abstractions.Interfaces;
+using Penguin.Cms.Pages;
+using Penguin.DependencyInjection.Abstractions.Interfaces;
+using Penguin.Reflection;
+using Penguin.Reflection.Extensions;
+using Penguin.Templating.Abstractions;
+using Penguin.Web.Templating;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TemplateParameter = Penguin.Templating.Abstractions.TemplateParameter;
+
+namespace Penguin.Cms.Modules.Pages.Rendering
+{
+    public class PageRenderer : ObjectRenderer, ISelfRegistering
+    {
+        protected static string ViewInjectors { get; set; }
+        protected IServiceProvider ServiceProvider { get; set; }
+        protected static object ViewInjectorLock = new object();
+
+        public PageRenderer(IHostingEnvironment hostingEnvironment, IServiceProvider serviceProvider = null) : base(hostingEnvironment)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public (string RelativePath, object Model) GenerateRenderInformation(Page page, IEnumerable<TemplateParameter> parameters = null)
+        {
+            if (page is null)
+            {
+                throw new System.ArgumentNullException(nameof(page));
+            }
+
+            parameters = parameters ?? new List<TemplateParameter>();
+
+            string PageContent;
+
+            lock (ViewInjectorLock)
+            {
+                if (ViewInjectors is null)
+                {
+                    StringBuilder viewInjectors = new StringBuilder();
+
+                    viewInjectors.Append(string.Empty);
+
+                    if (ServiceProvider != null)
+                    {
+                        foreach (Type t in TypeFactory.GetAllImplementations(typeof(IMacroProvider)))
+                        {
+                            if (ServiceProvider.GetService(t) != null)
+                            {
+                                viewInjectors.Append($"@inject {t.GetDeclaration()} {t.Name} {System.Environment.NewLine}");
+                            }
+                        }
+                    }
+
+                    ViewInjectors = viewInjectors.ToString();
+                }
+            }
+
+            PageContent = ViewInjectors + System.Environment.NewLine;
+
+            if (!string.IsNullOrWhiteSpace(page.Layout))
+            {
+                PageContent += "@{ Layout = \"" + page.Layout + "\"; }" + System.Environment.NewLine + System.Environment.NewLine;
+            }
+
+            PageContent += page.Content;
+
+            GeneratedTemplateInfo generatedTemplateInfo = base.GenerateTemplatePath(page, parameters, PageContent, "Content");
+
+            return (generatedTemplateInfo.RelativePath, generatedTemplateInfo.Model);
+        }
+    }
+}
